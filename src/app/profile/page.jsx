45232fa2b9eaee6a52bdbae38d312b040f115e4b
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-01-23 20:53:37 +0600

    private route done

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,10 +1,15 @@
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
+const page = async () => {
+  
 
-const page = () => {
+const {getUser} = getKindeServerSession();
+const user = await getUser();
+
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 text-center">
     
-    <h1 className="font-bold">protected route</h1>
+    <h1 className="font-bold">Welcome to your profile!  <span className="text-green-500 ">{user.given_name} </span></h1>
   </div>
   )
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,26 @@
 
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
 
-const Navbar = () => {
+const Navbar = async () => {
+  
+
+  const {getUser} = getKindeServerSession();
+  const user = await getUser();
   return (
     <header className="bg-gray-800 text-white p-4">
       <nav className="container mx-auto flex justify-between">
         <div className="flex items-center font-bold gap-3">
           <Link href="/">Home</Link>
-          <Link href="/profile">Profile</Link>
+          {
+            user.email ? <Link href="/profile">Profile</Link> : "" 
+          }
+         
         </div>
         <div className="font-semibold text-lg gap-3 flex hover:underline">
-          <button className=" text-red-500  px-1 py-2 ">Logout</button>
-          <button className="text-blue-600  px-1 py-2">Login</button>
+        {
+            user.email ?  <button className=" text-red-500  px-1 py-2 ">Logout</button> :  <Link href="/api/auth/login" className="text-blue-600  px-1 py-2">Login</Link>      
+          }
 
         </div>
       </nav>
