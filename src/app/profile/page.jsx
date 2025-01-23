350a9a240cfbd613cd50490d952bd07a6c256631
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-01-23 21:42:16 +0600

    auth and private route done

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -9,7 +9,7 @@ const user = await getUser();
   return (
     <div className="container mx-auto p-4 text-center">
     
-    <h1 className="font-bold">Welcome to your profile!  <span className="text-green-500 ">{user.given_name} </span></h1>
+    <h1 className="font-bold">Welcome to your profile!  <span className="text-green-500 ">{user?.given_name} </span></h1>
   </div>
   )
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -13,13 +13,13 @@ const Navbar = async () => {
         <div className="flex items-center font-bold gap-3">
           <Link href="/">Home</Link>
           {
-            user.email ? <Link href="/profile">Profile</Link> : "" 
+            user?.email ? <Link href="/profile">Profile</Link> : "" 
           }
          
         </div>
         <div className="font-semibold text-lg gap-3 flex hover:underline">
         {
-            user.email ?  <button className=" text-red-500  px-1 py-2 ">Logout</button> :  <Link href="/api/auth/login" className="text-blue-600  px-1 py-2">Login</Link>      
+            user?.email ?  <button className=" text-red-500  px-1 py-2 ">Logout</button> :  <Link href="/api/auth/login" className="text-blue-600  px-1 py-2">Login</Link>      
           }
 
         </div>
